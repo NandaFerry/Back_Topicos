474bfffaa07bf6ed4dc0ab940091ad0b2eedb3b8
Fernanda Ferry <nandateixferry@gmail.com>
2020-06-08 21:00:23 -0300

    Arquivos

---
+++ Projeto/VO/AnotacaoVO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto.VO
+{
+    public class AnotacaoVO : BaseVO
+    {
+        public string  Obs { set; get; }
+
+        public System.DateTime Data { set; get; }
+
+        public int? Id_pessoa_grupo { set; get; }
+    }
+}
